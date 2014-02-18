ecdb6cbe1a810c75d92b1f732d6eb550a5e8a4ec
Brekk Bockrath <brekk@watchwith.com>
2014-02-18 11:18:17 -0800

    Updated test.js

--- test/test.js
+++ test/test.js
@@ -26,4 +26,46 @@ describe('gulp-jst', function() {
     stream.write(fakeFile);
     stream.end();
   });
+  it('should compile Lodash template into string with namespace', function(done) {
+    var stream = compile({
+      namespace: 'ns.JST.hello'
+    });
+
+    var fakeBuffer = new Buffer('Hello <%= name %>!');
+    var fakeFile = new gutil.File({
+      contents: fakeBuffer
+    });
+
+    stream.on('data', function(file) {
+      assert.equal(file.contents.toString(), fs.readFileSync(__dirname + '/expected/hello-namespaced.js'));
+    });
+
+    stream.on('end', function() {
+      done();
+    });
+
+    stream.write(fakeFile);
+    stream.end();
+  });
+  it('should compile Lodash template into string with single namespace', function(done) {
+    var stream = compile({
+      namespace: 'hello'
+    });
+
+    var fakeBuffer = new Buffer('Hello <%= name %>!');
+    var fakeFile = new gutil.File({
+      contents: fakeBuffer
+    });
+
+    stream.on('data', function(file) {
+      assert.equal(file.contents.toString(), fs.readFileSync(__dirname + '/expected/hello-single-namespace.js'));
+    });
+
+    stream.on('end', function() {
+      done();
+    });
+
+    stream.write(fakeFile);
+    stream.end();
+  });
 });
